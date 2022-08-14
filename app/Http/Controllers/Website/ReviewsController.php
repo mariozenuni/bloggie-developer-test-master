881d0e7439c71c8b094acbd6b953c6cbcb178d1a
Mario Zenuni <mariozenuni@gm,ail>
2022-08-14 11:04:30 +0200

    implemeted frontend website section reviews

---
+++ app/Http/Controllers/Website/ReviewsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Website;
+
+use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use App\Models\Review;
+class ReviewsController extends Controller
+{
+
+    public function index()
+    {
+        $now = Carbon::now();
+        $reviews = Review::query()
+                ->whereNotNull('created_at')
+                ->where('created_at', '<=', $now)
+                ->orderBy('created_at', 'desc')
+                ->paginate(12);
+
+        return view('website.reviews.index')->with([
+            'reviews' => $reviews
+        ]);
+    }
+    
+}
---
+++ resources/views/website/reviews/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app', ['class' => 'bg-white'])
+
+@section('content')
+    <div class="header bg-gradient-primary py-3">
+        <div class="container">
+            <div class="header-body text-center mt-7 mb-7">
+                <div class="row justify-content-center">
+                    <div class="col-lg-5 col-md-6">
+                        <h1 class="text-white">
+                            Reviews
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container my-4">
+        @foreach ($reviews as $review)
+            <testimonial-card
+                class="mb-4 shadow-sm"
+                :testimonial="{{ json_encode($review->toArray()) }}"
+            ></testimonial-card>
+        @endforeach
+
+        <div class="mt-6">
+            {{ $reviews->links() }}
+        </div>
+    </div>
+
+    <latest-blogs></latest-blogs>
+@endsection
