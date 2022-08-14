1e5a24e63067888bb70d21eef9a0e32df9cc5a22
Mario Zenuni <mariozenuni@gm,ail>
2022-08-14 15:19:50 +0200

    fixed query

--- app/Http/Controllers/Api/BlogController.php
+++ app/Http/Controllers/Api/BlogController.php
@@ -29,17 +29,19 @@ class BlogController extends Controller
 
     public function latest(Request $request) : JsonResponse
     {
-        $now = Carbon::now();
-        $blogs = Blog::whereNotNull('published_at')
-                 ->where('published_at', '<=', $now)
-                 ->where(function ($subquery) use ($now) {
-                return $subquery->where('expired_at', '>', $now)->orWhereNull('expired_at');
+        $todayDate = date('Y-m-d');
+        $blogs = Blog::select('blogs.*')
+                 ->whereNotNull('published_at')
+                 ->where('published_at', '<', $todayDate)
+                 ->where(function ($subquery) use ($todayDate) {
+                return $subquery->where('expired_at', '>', $todayDate)
+                ->orWhereNull('expired_at');
             })
-            ->orderBy('published_at', 'desc')
+             ->orderBy('published_at','desc')
             ->limit($request->get('limit', 3))
             ->get();
-
         return response()->json($blogs);
     }
+    
 
 }
